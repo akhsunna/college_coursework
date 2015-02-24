2954ab4022e42b5806c4f6e0be97ec9e06035eb0
UserGraund <usergraund@gmail.com>
2015-02-24 23:38:23 +0200

    change admin, add tacher app, add url file write in views

---
+++ teacher/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+# Create your views here.
+
+def login(request):
+	form = AuthenticationForm(request, request.POST or None)
+	if request.POST:
+		if form.is_valid():
+			auth.login(request, form.get_user())
+			return redirect('/')
+		else:
+			return render(request, 'login.html', {'form': form})
+	else:
+		return render(request, 'login.html', {'form': form})
+
+def logout(request):
+	auth.logout(request)
+	return redirect('/')
