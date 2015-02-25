d2d1c09a969ac7c4cfb58b94a8e42dffe16cf24f
akhsunna <akhsunna4@gmail.com>
2015-02-25 20:05:31 +0200

    simple lists of lectures, PRs & LRs

--- information/urls.py
+++ information/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
 	url('^$',views.speciality_list, name = "speciality_list"), 
 	url(r'^(?P<speciality_id>\d+)/$', views.course_list, name='course_list'),
 	url(r'^(?P<speciality_id>\d+)/(?P<course_id>\d+)/$', views.subject_list, name='subject_list'),
+	url(r'^(?P<speciality_id>\d+)/(?P<course_id>\d+)/(?P<subject_id>\d+)/$', views.subject_show, name='subject_show'),
 )
--- information/views.py
+++ information/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from information.models import Speciality, CourseNumber, Subject
+from information.models import Speciality, CourseNumber, Subject, Lecture, PracticalWork
 
 def speciality_list(request):
 	specialities = Speciality.objects.all()
@@ -12,4 +12,12 @@ def course_list(request, speciality_id):
 
 def subject_list(request, speciality_id, course_id):
 	subjects = Subject.objects.filter(specialty=speciality_id,year=course_id)
-	return render(request,'subjects.html',{'subjects':subjects})
+	return render(request,'subjects.html',{'subjects':subjects,'course_id':course_id,'speciality_id':speciality_id})
+
+def subject_show(request, speciality_id, course_id, subject_id):
+	subject = Subject.objects.get(id=subject_id)
+	lectures = Lecture.objects.filter(subject=subject_id)
+	prs = PracticalWork.objects.filter(subject=subject_id,kind=1)
+	lrs = PracticalWork.objects.filter(subject=subject_id,kind=2)
+	return render(request,'subject.html',{'subject':subject,'lectures':lectures,'prs':prs,'lrs':lrs})
+
